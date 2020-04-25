0e3d1c4c8e109f54b79e5092dc97cacee46a57b5
FujiHaruka <fizzyjohn1427@gmail.com>
2020-04-26 00:21:20 +0900

    Define RoleObject type

--- packages/core/src/Application.type.ts
+++ packages/core/src/Application.type.ts
@@ -2,13 +2,14 @@ import { Sheet } from './Sheet.type'
 import { SheetGroup } from './SheetGroup.type'
 import { RepositoryDriver } from './Repository.type'
 
+export type RoleObject<Role extends string = string> = {
+  label: string
+  value: Role
+}
+
 export type Application<Role extends string> = {
   Sheets: readonly Sheet<any, Role>[]
-  Roles: readonly { label: string; value: Role }[]
+  Roles: readonly RoleObject[]
   Groups?: readonly SheetGroup[]
   DatabaseDriver: RepositoryDriver
 }
-
-export type ApplicationEndpoint = <Role extends string>() => Promise<
-  Application<Role>
->
