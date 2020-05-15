3cfc7486f8333d3510926bd65803995bca7f0eb9
FujiHaruka <fizzyjohn1427@gmail.com>
2020-05-15 10:06:04 +0900

    Fix type error in Link component

--- packages/ui/src/components/Link.tsx
+++ packages/ui/src/components/Link.tsx
@@ -1,14 +1,13 @@
-import React, { FC } from 'react'
+import React from 'react'
 import {
   Link as RouterLink,
   LinkProps as RouterLinkProps,
 } from 'react-router-dom'
 import MLink, { LinkProps as MLinkProps } from '@material-ui/core/Link'
 
-export const Link: FC<RouterLinkProps & MLinkProps> = React.forwardRef(
-  function Link(props, ref) {
-    return (
-      <MLink component={RouterLink} underline="none" {...props} ref={ref} />
-    )
-  },
-)
+export const Link = React.forwardRef(function Link(
+  props: RouterLinkProps & MLinkProps,
+  ref: any,
+) {
+  return <MLink component={RouterLink} underline="none" {...props} ref={ref} />
+})
