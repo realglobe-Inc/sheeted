85cc4f59fb368e2042be6affc6a1891aab64aef3
FujiHaruka <fizzyjohn1427@gmail.com>
2020-05-16 13:27:09 +0900

    Modify doc.sh

--- misc/scripts/doc.sh
+++ misc/scripts/doc.sh
@@ -2,6 +2,13 @@
 #
 # Generate documentations
 
+cd `dirname $0`/../..
+
+rm -rf doc/
+
+mkdir doc
+cp misc/doc/index.html doc/index.html
+
 # FIXME: --inputFiles は typedoc.js で指定しているので不要なはずだが、https://github.com/TypeStrong/typedoc/issues/1263 が修正されるまで引数で指定しないといけない
 PACKAGE=core     npx typedoc 'packages/core/src'
 PACKAGE=server   npx typedoc 'packages/server/src/index.ts' 'packages/server/src/types/ApplicationConfig.type.ts' 'packages/server/src/typings'
