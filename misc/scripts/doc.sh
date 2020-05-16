2bca1df2a494595226f89ca222a8289dada74823
FujiHaruka <fizzyjohn1427@gmail.com>
2020-05-16 16:58:02 +0900

    Deploy API reference to gh-pages branch

--- misc/scripts/doc.sh
+++ misc/scripts/doc.sh
@@ -4,10 +4,9 @@
 
 cd `dirname $0`/../..
 
-rm -rf doc/
-
-mkdir doc
-cp misc/doc/index.html doc/index.html
+mkdir -p doc
+cp misc/doc/index.html doc/
+cp misc/doc/.nojekyll doc/
 
 # FIXME: --inputFiles は typedoc.js で指定しているので不要なはずだが、https://github.com/TypeStrong/typedoc/issues/1263 が修正されるまで引数で指定しないといけない
 PACKAGE=core     npx typedoc 'packages/core/src'
