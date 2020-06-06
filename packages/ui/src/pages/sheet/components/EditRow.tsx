75e5a237455fea0c9a0a5aeea88e78744b8c5354
FujiHaruka <fizzyjohn1427@gmail.com>
2020-06-06 12:42:49 +0900

    Add beforeunload event while EditRow is on view

--- packages/ui/src/pages/sheet/components/EditRow.tsx
+++ packages/ui/src/pages/sheet/components/EditRow.tsx
@@ -1,12 +1,26 @@
 import React, { FC } from 'react'
 import { MTableEditRow } from 'material-table'
 
-import { useUnmountEffect } from '../../../hooks/utils/MountEffectHook'
+import {
+  useUnmountEffect,
+  useMountEffect,
+} from '../../../hooks/utils/MountEffectHook'
 import { useInputErrorContext } from '../hooks/InputErrorContextHook'
+
+const alertBeforeUnload = (e: BeforeUnloadEvent) => {
+  e.preventDefault()
+  e.returnValue = ''
+}
 
 export const EditRow: FC<any> = (props) => {
   // I'm waiting for being implemented onRowAddCancelled / onRowUpdateCancelled in material-table
   const { reset } = useInputErrorContext()
-  useUnmountEffect(reset)
+  useUnmountEffect(() => {
+    reset()
+    window.removeEventListener('beforeunload', alertBeforeUnload)
+  })
+  useMountEffect(() => {
+    window.addEventListener('beforeunload', alertBeforeUnload)
+  })
   return <MTableEditRow {...props} />
 }
