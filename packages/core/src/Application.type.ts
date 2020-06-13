c3e407cdc20593b18161378495be11d35131dd01
FujiHaruka <fizzyjohn1427@gmail.com>
2020-06-13 16:23:42 +0900

    Update document

--- packages/core/src/Application.type.ts
+++ packages/core/src/Application.type.ts
@@ -2,16 +2,25 @@ import { Sheet } from './Sheet.type'
 import { SheetGroup } from './SheetGroup.type'
 import { RepositoryDriver } from './Repository.type'
 
+/**
+ * Role label and value
+ */
 export type RoleObject<Role extends string = string> = {
   label: string
   value: Role
 }
 
+/**
+ * Server-defined API access token bound to a user
+ */
 export type ApiUser = {
   userId: string
   accessToken: string
 }
 
+/**
+ * Application object, which passes to server
+ */
 export type Application<Role extends string> = {
   AppTitle: string
   Sheets: readonly Sheet<any, Role>[]
