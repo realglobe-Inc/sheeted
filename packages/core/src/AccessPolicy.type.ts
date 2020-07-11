d142b4f01fdc921e0dcc528c08f652d6920fb9c9
FujiHaruka <fizzyjohn1427@gmail.com>
2020-07-11 18:11:54 +0900

    Update doc comments

--- packages/core/src/AccessPolicy.type.ts
+++ packages/core/src/AccessPolicy.type.ts
@@ -16,39 +16,94 @@ export type ColumnPolicyOption<Key = string> = {
   columns: Key[]
 }
 
+/**
+ * Access policy for reading.
+ */
 export type ReadAccessPolicy<Entity = any, Role extends string = string> = {
   action: 'read'
+
+  /**
+   * Role to be allowed the operation by the access policy.
+   */
   role: Role
+
+  /**
+   * Query filter
+   */
   queryFilter?: Partial<Entity>
+
+  /**
+   * Column setting
+   */
   column?: ColumnPolicyOption<keyof Entity>
 }
 
 export type CreateAccessPolicy<Entity = any, Role extends string = string> = {
   action: 'create'
+
+  /**
+   * Role to be allowed the operation by the access policy.
+   */
   role: Role
+
+  /**
+   * Column setting
+   */
   column?: ColumnPolicyOption<keyof Entity>
 }
 
 export type UpdateAccessPolicy<Entity = any, Role extends string = string> = {
   action: 'update'
+
+  /**
+   * Role to be allowed the operation by the access policy.
+   */
   role: Role
+
+  /**
+   * Column setting
+   */
   column?: ColumnPolicyOption<keyof Entity>
+
+  /**
+   * Condition to be allowed. The operation is allowed only when it returns true.
+   */
   condition?: (entity: Entity, context?: Context<Role>) => boolean
 }
 
 export type DeleteAccessPolicy<Entity = any, Role extends string = string> = {
   action: 'delete'
+
+  /**
+   * Role to be allowed the operation by the access policy.
+   */
   role: Role
+
+  /**
+   * Condition to be allowed. The operation is allowed only when it returns true.
+   */
   condition?: (entity: Entity, context?: Context<Role>) => boolean
 }
 
 export type ActionAccessPolicy<Entity = any, Role extends string = string> = {
   action: 'custom'
+
   customActionId: string
+
+  /**
+   * Role to be allowed the operation by the access policy.
+   */
   role: Role
+
+  /**
+   * Condition to be allowed. The operation is allowed only when it returns true.
+   */
   condition?: (entity: Entity, context?: Context<Role>) => boolean
 }
 
+/**
+ * Access policy for a sheet. This defines how roles are allowed to operate the sheet.
+ */
 export type AccessPolicy<Entity = any, Role extends string = string> =
   | ReadAccessPolicy<Entity, Role>
   | CreateAccessPolicy<Entity, Role>
