3e53f7e2d39d2c9b57bd89231d13903d0e3ed659
FujiHaruka <fizzyjohn1427@gmail.com>
2020-07-26 16:13:33 +0900

    Add documentation comments to Schema.type.ts

--- packages/core/src/Schema.type.ts
+++ packages/core/src/Schema.type.ts
@@ -2,15 +2,45 @@ import { EntityBase } from './EntityBase.type'
 import { Type } from './Type.type'
 
 export type SchemaField<E> = {
+  /**
+   * Field type.
+   */
   type: Type<E[keyof E]>
+
+  /**
+   * If set true, the field can be omitted.
+   */
   optional?: true
+
+  /**
+   * If set true, the field cannot be editted manually.
+   */
   readonly?: true
+
+  /**
+   * If set true, the field is contained in the search target.
+   */
   searchable?: true
+
+  /**
+   * If set true, the field value cannot be duplicated.
+   */
   unique?: true
+
+  /**
+   * Properties for Enum type.
+   */
   enumProperties?: {
     values: readonly string[]
   }
+
+  /**
+   * Properties for Entity type.
+   */
   entityProperties?: {
+    /**
+     * Sheet name of the referenced entity.
+     */
     sheetName: string
   }
 }
